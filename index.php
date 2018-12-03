75596c2865f4086161423e34675eef6357865f9e
Warren D. Rezane <warzkie123@gmail.com>
2018-12-04 02:27:46 +0800

    Added functionalities

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['staff_session'])) {
+    header('location: staff_management/navigation/index.php');
+}
+?>
 <!DOCTYPE html>
 <meta lang="utf-8">
 <meta name = "viewport" content = "width = device-width, initial-scale = 1.0">
@@ -17,21 +24,27 @@
 
                 <h3>Account login</h3>
 
-                <!-- Displays when invalid credentials are entered -->
-                <p class = "prompt" id = "error">
-                    <img src="images/error.png" alt="Error">
-                    Invalid username or password.
-                </p>
+                <?php 
+                if (isset($_SESSION['login_msg'])) {
+                    foreach($_SESSION['login_msg'] as $error) {
+                        echo   '<p class = "prompt" id = "error">
+                                    <img src="images/error.png" alt="Error">'
+                                    . $error .
+                                '</p>';
+                    }
+                    unset($_SESSION['login_msg']);
+                }
+                ?>
 
-                <form action="" method="">
+                <form action="includes/actions/login.php" method="POST">
 
                     <p class="form-indicator">Username</p>
-                    <input type="text" name="" id="" required autofocus>
+                    <input type="text" name="username" required autofocus>
 
                     <p class="form-indicator">Password</p>
-                    <input type="password" name="" id="" required>
+                    <input type="password" name="password" required>
 
-                    <button type="submit">Login</button>
+                    <button type="submit" name="login" value="login">Login</button>
 
                 </form>
 
